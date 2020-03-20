09dc231e56ef942aeeaa60c0ce78be83114f0df6
golovin961 <golovin961@gmail.com>
2020-03-20 12:06:44 +0300

    Init

--- Task5_1.hs
+++ Task5_1.hs
@@ -1,7 +1,5 @@
 module Task5_1 where
 
-import Todo(todo)
-
 -- Структура двусвязного списка из лекции про ленивость
 
 data DList a = DNil 
@@ -31,13 +29,35 @@ list2dlist' left (h: t) =
     let rec = DCons left h (list2dlist' rec t)
     in rec
 
-
 -- Реализуйте функции индексирования, вставки и удаления элементов
 index :: DList a -> Int -> a
-index = todo
+index d i
+  | i < 0         = error "Negative index"
+  | otherwise     = index' d i
+  where
+    index' DNil _ = error "Index too large"
+    index' d 0    = current d
+    index' d i    = index' (right d) (i - 1)
+
 
 insertAt :: DList a -> Int -> a -> DList a
-insertAt list index value = todo
+insertAt = insertAt' DNil
+      where
+        insertAt' :: DList a -> DList a -> Int -> a -> DList a
+        insertAt' left DNil i v
+          | i == 0    = DCons left v DNil
+          | otherwise = error "Index Out Of Bound Exception"
+        insertAt' left (DCons l x r) i v
+          | i == 0    = let rec = DCons l v (DCons rec x r) in rec
+          | otherwise = let rec = DCons l x (insertAt' rec r (i - 1) v) in rec
 
 removeAt :: DList a -> Int -> DList a
-removeAt list index = todo
+removeAt list index
+  | index < 0 = error "Negative index"
+  | otherwise = removeAt' DNil list index
+    where
+      removeAt' _ DNil index = if index < 0 then DNil else error "Index too large"
+      removeAt' l list 0 = removeAt' l (right list) (-1)
+      removeAt' l list index =
+        let rec = DCons l (current list) (removeAt' rec (right list) (index - 1))
+        in rec
